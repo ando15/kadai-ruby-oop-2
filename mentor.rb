3c8d519d93b59667e1ab4b69a0261806cb6c7ea0
tomoya.ando <tomoya.ando@techacademy.jp>
2022-08-05 08:04:29 +0000

    ruby-oop-2

---
+++ mentor.rb
@@ -0,0 +1,23 @@
+class Mentor
+  attr_accessor :name
+  
+  def initialize(name)
+    self.name = name
+  end
+  
+  def job
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+  end
+end
+
+class RailsMentor < Mentor
+  def job
+    puts "#{self.name}です。私はRubyとRailsでwebアプリケーションを作ります。"
+  end
+end
+
+mentor_A = Mentor.new("煌木")
+railsmentor_A = RailsMentor.new("赤木")
+
+mentor_A.job
+railsmentor_A.job
